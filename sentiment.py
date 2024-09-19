4778f6460e0e12221578156ac964da8cd8bdbeb8
Parth <parthdande15@gmail.com>
2024-09-19 22:57:28 +0530

    api switch
    Due to contraints for free deployments
    loading models in application in resource intensive hence switching to api approach. All models will get called using huggingface api

--- sentiment.py
+++ sentiment.py
@@ -1,26 +1,26 @@
+from huggingface_hub import InferenceClient
+
+
 from flask import Flask, request, render_template
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
 import time
 import random
-
-nltk.download('vader_lexicon', quiet=True)
-
 app = Flask(__name__)
 class TextAnalysis:
     @staticmethod
     def analyze_sentiment(text):
-        sia = SentimentIntensityAnalyzer()
-        sentiment_scores = sia.polarity_scores(text)
-            
-        compound_score = sentiment_scores['compound']
-            
-        if compound_score >= 0.05:
-                return "Positive", abs(compound_score) * 100
-        elif compound_score <= -0.05:
-                return "Negative", abs(compound_score) * 100
-        else:
-                return "Neutral", (1 - abs(compound_score)) * 100
+        
+        client = InferenceClient(
+            model="cardiffnlp/twitter-roberta-base-sentiment-latest",
+            token="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"
+        )
+
+        # Perform sentiment analysis
+        result = client.text_classification(text)
+
+        # Extract sentiment and score with the highest score
+        sentiment = result[0]['label']
+        score = result[0]['score']
+        return sentiment,score
 
 
 @app.route('/sentiment', methods=['GET', 'POST'])
@@ -33,9 +33,9 @@ def index():
         return {
             'text': text,
             'sentiment': sentiment,
-            'confidence': round(confidence, 2)
+            'confidence': round(confidence * 100, 2)  # Convert to percentage and round to 2 decimal places
         }
-    return render_template('sentiment.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
