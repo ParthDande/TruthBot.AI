1c646090cff9772d5709671e5d954ecddbaa8f38
Parth <parthdande15@gmail.com>
2024-09-19 13:01:27 +0530

    host set to '0.0.0.0'
    set host to '0.0.0.0' so that the web app can run on other devices

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def news_summarization():
         return render_template("summarization.html", summary="")
 if __name__ == "__main__": 
     obj = TextAnalysis()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug =True)
